181cbf88e19ed86f6f1065399662a687cabda54b
ARashitov <adil.rashitov.98@gmail.com>
2023-03-05 17:23:58 +0600

    feat: adding exploratory notebooks

--- src/path.py
+++ src/path.py
@@ -1,2 +1,3 @@
-DIR_01_RAW_PLANS = "../data/01_raw/plans_anonymized.parquet"
-DIR_01_RAW_GPS = "../data/01_raw/gps_anonymized/"
+FILE_01_RAW_PLANS = "../data/01_raw/plans_anonymized.parquet"
+FILE_01_RAW_GPS = "../data/01_raw/gps_anonymized.parquet"
+FILE_01_RAW_GEOCODES = "../data/01_raw/geocodes/locations.csv"
